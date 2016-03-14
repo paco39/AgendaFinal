62953f34245fa74ce258591019e0a5301d595347
renato7vargas <renaswat@gmail.com>
2016-03-14 11:16:20 -0600

    Update TipoBusqueda.java

--- AgendaFinal/src/modelo/TipoBusqueda.java
+++ AgendaFinal/src/modelo/TipoBusqueda.java
@@ -1,5 +1,8 @@
 package modelo;
-
+/**
+* Establece constantes predefinidas para
+* ser asignadas a las variables
+*/
 public enum TipoBusqueda {
     porNombre, porCorreo, porOcupacion, porFechaNacimiento
     
